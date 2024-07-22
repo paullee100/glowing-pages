da49acaef0905cafeeb9213717146ef454476628
paullee100 <leepaul863@gmail.com>
2024-07-22 14:37:51 -0700

    Update page.tsx

--- src/app/recipes/[items]/page.tsx
+++ src/app/recipes/[items]/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from "./recipeItemPage.module.css";
 
+export function generateStaticParams() {
+  return [];
+}
+
 const RecipeItemPage = () => {
   return (
     <div>RecipeItemPage</div>
