7d2182e30359728d2b56d1731b5ae22b5b2e6160
paullee100 <leepaul863@gmail.com>
2025-04-12 15:55:29 -0700

    updates

---
+++ src/app/api/auth/[...nextauth]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const dynamicParams = true
+export async function generateStaticParams() {
+  return []
+}
+
+const AuthPage = () => {
+  return (
+    <div>AuthPage</div>
+  )
+}
+
+export default AuthPage
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,1 @@
-export const dynamicParams = true
-export async function generateStaticParams() {
-  return []
-}
-
 export { GET, POST } from "@/lib/auth"
